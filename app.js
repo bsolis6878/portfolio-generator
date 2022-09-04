b2c5253e99f1d0de3da3a10fd429c9e51a383e8c
bsolis6878 <bsolis6878@gmail.com>
2022-09-04 18:20:06 -0500

    can enter multiple projects into array if add another is selected

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ const promptUser = () => {
         ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
+
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
     console.log(`
     =================
     Add a New Project
@@ -57,16 +62,25 @@ const promptProject = () => {
         {
             type: 'confirm',
             name: 'confirmAddProject',
-            message: 'Would you like to ender another project?',
+            message: 'Would you like to enter another project?',
             default: false
         }
-    ]);
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
 };
 
 promptUser()
-    .then(answers => console.log(answers))
     .then(promptProject)
-    .then(projectAnswers => console.log(projectAnswers));
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
 
 
 // const fs = require('fs');
